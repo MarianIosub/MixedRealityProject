0ad154e097c0eb9c9f3f2fa3b550e66ed362ed7a
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2021-12-29 11:31:30 +0200

    updated tower creation

--- JengaVR/Assets/Scripts/CreateTower.cs
+++ JengaVR/Assets/Scripts/CreateTower.cs
@@ -11,7 +11,7 @@ public class CreateTower : MonoBehaviour
     public Material oakMaterial, pineMaterial, mahagonyMaterial;
 
     private Transform tower;
-    
+
     /*
     private void AddEventListenersOnTowerCubes(GameObject tower)
     {
@@ -27,36 +27,39 @@ public class CreateTower : MonoBehaviour
         if (PhotonNetwork.IsMasterClient)
         {
             tower = PhotonNetwork.Instantiate(towerPrefab.name, new Vector3(0, 0.49f, -0.2f), Quaternion.identity);
+        }
 
-
-            if (tower is not null)
+        if (tower is not null)
+        {
+            for (int i = 0; i < 54; i++)
             {
-                for (int i = 0; i < 54; i++)
+                tower.transform.GetChild(i).GetComponent<Rigidbody>().mass = 5;
+
+                var settings = File.ReadLines("Assets/Settings/settings.txt");
+                switch (settings.ElementAt(1).Split("=")[1])
                 {
-                    tower.transform.GetChild(i).GetComponent<Rigidbody>().mass = 5;
-
-                    var settings = File.ReadLines("Assets/Settings/settings.txt");
-                    switch (settings.ElementAt(1).Split("=")[1])
-                    {
-                        case "0":
-                            tower.transform.GetChild(i).GetComponent<MeshRenderer>().material = oakMaterial;
-                            break;
-                        case "1":
-                            tower.transform.GetChild(i).GetComponent<MeshRenderer>().material = pineMaterial;
-                            break;
-                        case "2":
-                            tower.transform.GetChild(i).GetComponent<MeshRenderer>().material = mahagonyMaterial;
-                            break;
-                    }
+                    case "0":
+                        tower.transform.GetChild(i).GetComponent<MeshRenderer>().material = oakMaterial;
+                        break;
+                    case "1":
+                        tower.transform.GetChild(i).GetComponent<MeshRenderer>().material = pineMaterial;
+                        break;
+                    case "2":
+                        tower.transform.GetChild(i).GetComponent<MeshRenderer>().material = mahagonyMaterial;
+                        break;
                 }
-
-                //AddEventListenersOnTowerCubes(tower);
-                tower.AddComponent<TowerCubeEvent>();
             }
+        }
 
-            this.GetComponent<PhotonView>().RPC("UpdateTower", RpcTarget.Others, tower);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            //AddEventListenersOnTowerCubes(tower);
+            tower.AddComponent<TowerCubeEvent>();
         }
     }
+
+    // this.GetComponent<PhotonView>().RPC("UpdateTower", RpcTarget.Others, tower);
+
 
     // Update is called once per frame
     void Update()
