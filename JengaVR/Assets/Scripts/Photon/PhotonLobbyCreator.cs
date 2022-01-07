3daeb00cf25f289a445596623d43f44350358616
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2022-01-07 14:40:48 +0200

    Button for back funtional

--- JengaVR/Assets/Scripts/Photon/PhotonLobbyCreator.cs
+++ JengaVR/Assets/Scripts/Photon/PhotonLobbyCreator.cs
@@ -55,7 +55,7 @@ public class PhotonLobbyCreator : MonoBehaviourPunCallbacks
 
         if (PlayerNickname.Length == 0)
         {
-            PlayerNickname = "NoName";
+            PlayerNickname = "Player";
         }
 
         PhotonNetwork.LocalPlayer.NickName = PlayerNickname;
